0e48c2f9dfe29d95b424f29bb3b798d1d7c869d0
willys_fueguino <lokillof@gmail.com>
2022-04-19 10:54:42 -0300

    Modificacion BD

--- config/db.js
+++ config/db.js
@@ -2,8 +2,13 @@ const chalk = require("chalk")
 const mongoose = require("mongoose")
 //const {Schema, Model} = require("mongoose");
 //const url = `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_NAME}`;
-const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
+let environment = process.env.NODE_ENV
 
+let url = `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
+
+if(environment === "cloud"){
+  url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+}
 
 let connectDB = async function(){
 
