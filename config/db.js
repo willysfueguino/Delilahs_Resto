699b446244f4f3d91c304d20f0e84c1734665255
willys_fueguino <lokillof@gmail.com>
2022-04-19 13:33:33 -0300

    Correciones de DB

--- config/db.js
+++ config/db.js
@@ -7,13 +7,13 @@ let environment = process.env.NODE_ENV
 let url = `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
 
 if(environment === "cloud"){
-  url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+  url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@delilahs-cluster.sblbj.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 }
 
 let connectDB = async function(){
 
   try{ 
-    
+    //console.log(url)
     await mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }, (err, res)=> {
     console.log('Conectado a la base de datos')   
     })
@@ -23,6 +23,3 @@ let connectDB = async function(){
 }
 
 connectDB()
-
-
-
